c5c9425a8736aec4ecf8e826b110943b85d3fbb3
Developer Rahul <83329806+developerrahulofficial@users.noreply.github.com>
2023-05-11 10:43:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const editableText= document.getElementById('editable-text');
+
+if(localStorage.getItem('savedText')){
+  editableText.textContent=localStorage.getItem('savedText');
+}
+
+
+editableText.addEventListener('input', function(){
+
+  localStorage.setItem('savedText',this.textContent);
+})
